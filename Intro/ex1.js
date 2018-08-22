36d1c2b411e9e1a0bbcacb167f0591e2ee278a5b
anagalakshmi.ip <anagalakshmi.ip@gmail.com>
2018-08-22 23:36:39 +0530

    first-commit

--- Intro/ex1.js
+++ Intro/ex1.js
@@ -1,26 +1,3 @@
-var http = require('http');
-var formidable = require('formidable');
-var fs = require('fs');
-
-http.createServer(function (req, res) {
-    if (req.url == '/fileupload') {
-        var form = new formidable.IncomingForm();
-        form.parse(req, function (err, fields, files) {
-            var oldpath = files.filetoupload.path;
-            var newpath = 'C:/Users/INNOVAPATH.INC/Desktop/Newfolder' + files.filetoupload.name;
-            fs.rename(oldpath, newpath, function (err) {
-                if (err) throw err;
-                res.write('File uploaded and moved!');
-                res.end();
-            });
-        });
-    } else {
-        res.writeHead(200, {'Content-Type': 'text/html'});
-        res.write('<form action="fileupload" method="post" enctype="multipart/form-data">');
-        res.write('<input type="file" name="filetoupload"><br>');
-        res.write('<input type="submit">');
-        res.write('</form>');
-        return res.end();
-    }
-}).listen(8080);
-
+//var Buffer=require('buffer') ;
+const buf2 = Buffer.alloc(10,'a');
+console.log(buf2);
